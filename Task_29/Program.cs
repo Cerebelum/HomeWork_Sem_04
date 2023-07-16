8f5ff0984790871531e196df436a75809a841402
Michael_Fesenko <@gmail.com>
2023-07-16 17:15:10 +0300

    добавил Task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] GenerateRandomArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(10, 100);
+    }
+    return array;
+}
+
+int[] mas = GenerateRandomArray(8);
+PrintArray(mas);
